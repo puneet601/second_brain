48b0adb7090fa2566eb2cbef1262259afb6e5903
Puneet Jattana <puneet.jattana@thoughtworks.com>
2025-12-19 12:26:03 +0530

    feat: Add otel for monitoring and observability

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from core.observability import init_observability
 from core.orchestrator import Orchestrator
 
+from opentelemetry import trace
+from otel_setup import setup_tracing
 
 def main():
     print("Hello from your second-brain! The bot is ready to chat. Please reply with /exit to quit")
-    query = input("You:")
+    setup_tracing()
+    tracer = trace.get_tracer(__name__)
+
+    with tracer.start_as_current_span("orchestrator.run"):
+        app = Orchestrator()
+        app.run()
 
 
 
 if __name__ == "__main__":
-    init_observability(local_only=True)
-    app = Orchestrator()
-    app.run()
+    main()
